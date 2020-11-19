230f5618c5449abc42df01c655a0e9f8cdfe94ee
Luong Pham Bao <19521242@gm.uit.edu.vn>
2020-11-19 10:06:59 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.set("view engine","ejs");
+const CartRoute = require('./route/shoppingCart');
+const UserRoute = require('./route/user');
+ const HomepageRoute = require('./route/homepage');
+app.get("/",(req,res,next)=>{
+    res.status(200).json({
+        status:"SUCCESS",
+        message:"Welcome to shop",
+        error:null
+    })
+});
+app.get("/alo",(req,res,next)=>{
+    res.render("Homepage");
+})
+app.use("/homepage",HomepageRoute);
+app.use('/Cart',CartRoute);
+app.use('/register',UserRoute);
+app.get("*",(req,res,next) =>{
+    res.status(404).json({
+        status:"FAILED",
+        message:"API not found"
+    });
+});
+module.exports = app;
